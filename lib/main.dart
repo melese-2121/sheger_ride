ab864c0f3566e429690bd8f4e2757f53ecf6c0d0
melese-2121 <meleseayen2016@gmail.com>
2025-08-27 15:30:39 +0300

    feat: .dnv for supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:sheger_ride/screens/splash_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env.local");
+
   await Supabase.initialize(
-    url: 'https://YOUR_PROJECT_ID.supabase.co',  // replace with your Supabase project URL
-    anonKey: 'YOUR_PUBLIC_ANON_KEY',             // replace with your Supabase anon key
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
 
   runApp(const MyApp());
